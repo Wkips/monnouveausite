15ff08c8eabaa3f22bbd03432e141a4ecf2a7dc5
Wkips <willy_kips@hotmail.fr>
2023-03-14 16:50:11 +0100

    start thememika

--- wp-content/themes/thememika/index.php
+++ wp-content/themes/thememika/index.php
@@ -3,4 +3,4 @@
     <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width-device-width, initial-scale-1.0">
-            <title> Theme mika </title>
+            <title> Theme mika </title> 
